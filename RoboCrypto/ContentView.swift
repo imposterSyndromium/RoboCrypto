75874b7a324db7cea80b392a904576f94e3c389c
Robin OBrien <robin.c.obrien@gmail.com>
2025-03-01 03:39:01 -0500

    Initial Commit

---
+++ RoboCrypto/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RoboCrypto
+//
+//  Created by Robin O'Brien on 2025-03-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RoboCrypto/RoboCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoboCryptoApp.swift
+//  RoboCrypto
+//
+//  Created by Robin O'Brien on 2025-03-01.
+//
+
+import SwiftUI
+
+@main
+struct RoboCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
